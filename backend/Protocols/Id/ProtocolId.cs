95ff0e6e7a29e348c7ce4aad6e857f2f0bd6adb8
Elky (SeongHoon Kim) <elky84@gmail.com>
2022-01-27 15:19:09 +0900

    ProtocolId에 readonly 프로퍼티 추가

--- backend/Protocols/Id/ProtocolId.cs
+++ backend/Protocols/Id/ProtocolId.cs
@@ -4,19 +4,19 @@
     : EzAspDotNet.Protocols.Id.ProtocolId
     {
         // Auth
-        public static ProtocolId Authenticate = new(1, nameof(Authenticate));
-        public static ProtocolId AuthCheck = new(2, nameof(AuthCheck));
+        public static readonly ProtocolId Authenticate = new(1, nameof(Authenticate));
+        public static readonly ProtocolId AuthCheck = new(2, nameof(AuthCheck));
 
-        public static ProtocolId SignUp = new(10, nameof(SignUp));
-        public static ProtocolId SignIn = new(11, nameof(SignIn));
-        public static ProtocolId SignOut = new(12, nameof(SignOut));
-        public static ProtocolId AccountUpdate = new(13, nameof(AccountUpdate));
+        public static readonly ProtocolId SignUp = new(10, nameof(SignUp));
+        public static readonly ProtocolId SignIn = new(11, nameof(SignIn));
+        public static readonly ProtocolId SignOut = new(12, nameof(SignOut));
+        public static readonly ProtocolId AccountUpdate = new(13, nameof(AccountUpdate));
 
         // Board
-        public static ProtocolId Article = new(20, nameof(Article));
-        public static ProtocolId ArticleList = new(21, nameof(ArticleList));
-        public static ProtocolId Comment = new(22, nameof(Comment));
-        public static ProtocolId CommentList = new(23, nameof(CommentList));
+        public static readonly ProtocolId Article = new(20, nameof(Article));
+        public static readonly ProtocolId ArticleList = new(21, nameof(ArticleList));
+        public static readonly ProtocolId Comment = new(22, nameof(Comment));
+        public static readonly ProtocolId CommentList = new(23, nameof(CommentList));
 
         public ProtocolId(int id, string name)
             : base(id, name)
